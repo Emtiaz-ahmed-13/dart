7f8481af63a6b29d27e39685202a2a7e01c93239
Emtiaz Ahmed <emtiaz.ahmed@g.bracu.ac.bd>
2026-04-13 11:57:24 +0600

    variable done

---
+++ variable.dart
@@ -0,0 +1,33 @@
+void main() {
+  //1. numbers;
+  num MyNumber = 10;
+  num age = 15;
+  num anotherNumber = 15.45;
+
+  print(MyNumber);
+  print(age);
+  print(anotherNumber);
+
+  //2. strings
+
+  String firstName = "EMTIAZ";
+  String lastName = "Ahmed";
+  String fullname = firstName + lastName; //string concatenation..
+
+  print(fullname);
+  print('My name is $fullname'); //string interpolation
+
+  //3 booleans:
+
+  bool isActive = false;
+  print("Is she avaiable now= $isActive");
+
+  var mynum = 'Hello';
+  print(mynum);
+
+  var favoNumber = 7;
+  var anoNumber = 4.55;
+
+  print(favoNumber);
+  print(anoNumber);
+}
